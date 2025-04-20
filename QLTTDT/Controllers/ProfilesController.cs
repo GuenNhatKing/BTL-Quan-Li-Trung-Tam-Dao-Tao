1e0a060e41a73d1253476a33650dd578f6a5d99f
Guen Nhat King <guennhatking@gmail.com>
2025-04-20 19:29:37 +0700

    Fix change profiles

--- QLTTDT/Controllers/ProfilesController.cs
+++ QLTTDT/Controllers/ProfilesController.cs
@@ -20,7 +20,7 @@ namespace QLTTDT.Controllers
         }
         public async Task<IActionResult> Index(string username, int? id)
         {
-            if (username == null || id == null) 
+            if (username == null || id == null)
                 return NotFound();
             var user = await _context.NguoiDungs.FirstOrDefaultAsync(i => i.MaNguoiDung == id);
             if (user == null)
@@ -34,15 +34,15 @@ namespace QLTTDT.Controllers
                 SoDienThoai = user.SoDienThoai,
                 UrlAnhDaiDien = user.UrlAnhDaiDien,
                 SoKhoaHocDaDangKi = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung),
-                SoKhoaHocDangHoc = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung 
+                SoKhoaHocDangHoc = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung
                 && _context.KhoaHocs.FirstOrDefault(j => j.MaKhoaHoc == i.MaKhoaHoc).ThoiGianKhaiGiang <= DateTime.Now && i.TienDo < 100),
-                SoKhoaHocDaHoanThanh = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung  && i.TienDo == 100),
+                SoKhoaHocDaHoanThanh = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung && i.TienDo == 100),
             };
             return View(profiles);
         }
         public async Task<IActionResult> ChangeProfiles(string username, int? id)
         {
-            if (username == null || id == null) 
+            if (username == null || id == null)
                 return NotFound();
             var user = await _context.NguoiDungs.FirstOrDefaultAsync(i => i.MaNguoiDung == id);
             if (user == null)
@@ -56,16 +56,16 @@ namespace QLTTDT.Controllers
                 SoDienThoai = user.SoDienThoai,
                 UrlAnhDaiDien = user.UrlAnhDaiDien,
                 SoKhoaHocDaDangKi = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung),
-                SoKhoaHocDangHoc = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung 
+                SoKhoaHocDangHoc = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung
                 && _context.KhoaHocs.FirstOrDefault(j => j.MaKhoaHoc == i.MaKhoaHoc).ThoiGianKhaiGiang <= DateTime.Now && i.TienDo < 100),
-                SoKhoaHocDaHoanThanh = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung  && i.TienDo == 100),
+                SoKhoaHocDaHoanThanh = await _context.DangKiKhoaHocs.CountAsync(i => i.MaHocVien == user.MaNguoiDung && i.TienDo == 100),
             };
             return View(profiles);
         }
         [HttpPost]
         public async Task<IActionResult> ChangeProfiles(string username, int? id, Profiles profiles, IFormFile? AnhDaiDien = null)
         {
-            if (username == null || id == null) 
+            if (username == null || id == null)
                 return NotFound();
             var user = await _context.NguoiDungs.FirstOrDefaultAsync(i => i.MaNguoiDung == id);
             if (user == null)
@@ -75,20 +75,22 @@ namespace QLTTDT.Controllers
             {
                 try
                 {
-                    var imageUpload = new ImageUpload(_webHost);
-                    if (await imageUpload.SaveImageAs(AnhDaiDien))
-                    {
-                        imageUpload.DeleteImage(user.UrlAnhDaiDien);
-                        user.UrlAnhDaiDien = imageUpload.FileName;
-                    }
                     user.HoVaTen = profiles.HoVaTen;
                     user.NgaySinh = profiles.NgaySinh;
                     user.Email = profiles.Email;
                     user.SoDienThoai = profiles.SoDienThoai;
                     _context.Update(user);
                     await _context.SaveChangesAsync();
+                    var imageUpload = new ImageUpload(_webHost);
+                    if (await imageUpload.SaveImageAs(AnhDaiDien))
+                    {
+                        imageUpload.DeleteImage(user.UrlAnhDaiDien);
+                        user.UrlAnhDaiDien = imageUpload.FileName;
+                    }
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateConcurrencyException ex)
                 {
                     if (!NguoiDungExists(user.MaNguoiDung))
                     {
@@ -96,7 +98,7 @@ namespace QLTTDT.Controllers
                     }
                     else
                     {
-                        throw;
+                        return BadRequest(ex.Message);
                     }
                 }
                 return RedirectToAction("Index", new { username = username, id = id });
